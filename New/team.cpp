738646ec4577230f20df5f0674f8856b9ac49b04
Arafat Ali Shaon <140944047+Shaon22235103054@users.noreply.github.com>
2023-08-06 12:33:46 +0600

    Add files via upload

---
+++ New/team.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, a, c, v, b, cnt = 0;
+    cin >> v;
+
+    for (i = 0; i < v; i++)
+        {
+            cin >> a >> b >> c;
+            if ((a == 1 && b == 1 && c == 1) || (a == 0 && b == 1 && c == 1) || (a == 1 && b == 1 && c == 0) || (a == 1 && b == 0 && c == 1))
+                {
+                    cnt = cnt + 1;
+                }
+        }
+
+    cout << cnt;
+    return 0;
+}
+
